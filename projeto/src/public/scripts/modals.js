0982ccfa14055cd0fe0cbcca23eb45e6fbfb01db
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2021-03-27 17:24:41 -0300

    Adição do modal de dúvidas frequentes na tela de resultado da busca

--- projeto/src/public/scripts/modals.js
+++ projeto/src/public/scripts/modals.js
@@ -2,4 +2,7 @@ const Modal = {
     toggleZoom() {
       document.querySelector(".modal-overlay").classList.toggle("active");
     },
+    toggleHelp() {
+      document.querySelector(".modal-overlay-help").classList.toggle("active");
+    },
   };
