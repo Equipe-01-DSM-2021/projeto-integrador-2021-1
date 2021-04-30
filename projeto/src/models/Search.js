2e891e5146a921624087b74678dd26ca6ee970f0
Giovana O. Silva <78885451+gioliveirass@users.noreply.github.com>
2021-04-30 14:55:02 -0300

    Adição dos gráficos nos CARDS quando uma pesquisa é feita
    O script do gráfico já está no próprio index.ejs, que recebe quais dados devem ser exibidos em cada CARD. Os scripts separados para cada gráfico sem zoom que estavam em src/views/static foram apagados, pois não serão mais utilizados.

--- projeto/src/models/Search.js
+++ projeto/src/models/Search.js
@@ -5,50 +5,87 @@ const cards = [
   {
     id: 1,
     canvasId: "g-rep-eleito",
-    scriptGraphic: "../src/",
     title: "Representante eleito",
     description: "Legendas e descrição",
-    zoomGraphic: "../static/rep-eleito-zoom.js",
+    /* DADOS DO GRÁFICO */
+    labels: [
+      'Fulano', 
+      'Ciclano', 
+      'Beltrano'
+    ],
+    data: [46, 134, 88],
   },
   {
     id: 2,
     canvasId: "g-escolaridade",
-    scriptGraphic: "../static/escolaridade-graphic.js",
     title: "Escolaridade",
     description: "Legendas e descrição",
-    zoomGraphic: "../static/escolaridade-zoom.js",
+    /* DADOS DO GRÁFICO */
+    labels: [
+      'Analfabeto', 
+      'Lê e escreve', 
+      'Fundamental completo', 
+      'Fundamental incompleto', 
+      'Médio completo', 
+      'Médio incompleto', 
+      'Superior completo', 
+      'Superior incompleto'
+    ],
+    data: [33, 33, 43, 33, 35, 33, 23, 31],
   },
   {
     id: 3,
     canvasId: "g-genero",
-    scriptGraphic: "../static/genero-graphic.js",
     title: "Gênero",
     description: "Legendas e descrição",
-    zoomGraphic: "../static/genero-zoom.js",
+    /* DADOS DO GRÁFICO */
+    labels: [
+      'Feminino', 
+      'Masculino'
+    ],
+    data: [100, 97],
   },
   {
     id: 4,
     canvasId: "g-estado-civil",
-    scriptGraphic: "../static/estado-civil-graphic.js",
     title: "Estado civil",
     description: "Legendas e descrição",
-    zoomGraphic: "../static/estado-civil-zoom.js",
+    /* DADOS DO GRÁFICO */
+    labels: [
+      'Solteiro', 
+      'Casado', 
+      'Viúvo', 
+      'Separado judicialmente', 
+      'Divorciado'
+    ],
+    data: [34, 44, 54, 44, 44],
   },
   {
     id: 5,
     canvasId: "g-nomsocial-def",
-    scriptGraphic: "../static/nomsocial-def-graphic.js",
     title: "Possuintes de nome social ou deficiência",
     description: "Legendas e descrição",
-    zoomGraphic: "../static/nomsocial-def-zoom.js",
+    /* DADOS DO GRÁFICO */
+    labels: [
+      'Nome social', 
+      'Deficiência'
+    ],
+    data: [54,34],
   },
   {
     id: 6,
     canvasId: "g-faixa-etaria",
-    scriptGraphic: "../static/faixa-etaria-graphic.js",
     title: "Faixa etária",
     description: "Legendas e descrição",
-    zoomGraphic: "../static/faixa-etaria-zoom.js",
+    /* DADOS DO GRÁFICO */
+    labels: [
+      '16 a 18 anos', 
+      '19 a 29 anos', 
+      '30 a 49 anos', 
+      '50 a 59 anos', 
+      '60 anos ou mais'
+    ],
+    data: [54,52,42,52,62],
   },
 ];
 
