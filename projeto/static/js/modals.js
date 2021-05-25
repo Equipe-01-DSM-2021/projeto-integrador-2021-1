a055640294b35e1304e84e718c6f56235dcd296f
Giovana O. Silva <78885451+gioliveirass@users.noreply.github.com>
2021-05-25 17:24:32 -0300

    Adição da função de dar zoom nos gráficos
    Foi adicionada a função de visualizar os gráficos em um tamanho maior: toggleZoom( ), em modals.js

--- projeto/static/js/modals.js
+++ projeto/static/js/modals.js
@@ -1,6 +1,52 @@
 const Modal = {
-  toggleZoom() {
+  toggleZoom(chartCod, chartTitle) {
+    // Ativação do modal
     document.querySelector(".modal-overlay").classList.toggle("active");
+
+    // adicionando o canvas
+    canvas = document.getElementById("canvas");
+    canvas.innerHTML = '';
+    canvas.innerHTML = '<canvas id="chart-zoom" width="auto" height="auto"></canvas>';
+
+    // configurando o modal
+    title = document.getElementById("chartTitle");
+    title.innerHTML = chartTitle;
+    console.log();
+
+    // criando o gráfico
+    Chart.defaults.global.defaultFontColor = "#50525f";
+    var ctx = document.getElementById('chart-zoom').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: global.dataKeys[chartCod],
+            datasets: [{
+                label: 'Total',
+                data: global.dataValues[chartCod],
+                backgroundColor: 'rgba(154, 219, 249, 0.4)',
+                borderColor: 'rgba(154, 219, 249)',
+                borderWidth: 1,
+            }]
+        },
+        options: {
+            /* Desativa a legenda */
+            legend: {
+                display: false
+            },
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }],
+            },
+        }
+    });
+
+
+
+
+
   },
   toggleHelp() {
     document.querySelector(".modal-overlay-help").classList.toggle("active");
