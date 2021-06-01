1ff87fc603ad197a53fe18e56f5f62c634dc0eab
Giovana O. Silva <78885451+gioliveirass@users.noreply.github.com>
2021-06-01 15:08:30 -0300

    Ajustes na busca e adição da porcentagem do eleitorado jovem/idoso
    Foi feita a lógica para não permitir uma pesquisa sem preencher todos os selects, e foi adicionada a porcentagem nos gráficos de comparação de região por eleitorado mais jovem/idoso.

--- projeto/static/js/modals.js
+++ projeto/static/js/modals.js
@@ -1,7 +1,7 @@
 const Modal = {
 
   // MODAL ZOOM
-  toggleZoom(chartCod, chartTitle, labelStringY, chartType) {
+  toggleZoom(chartCod, chartTitle, labelStringY, chartType, comparison) {
     // Ativação do modal
     document.querySelector(".modal-overlay").classList.toggle("active");
 
@@ -15,39 +15,102 @@ const Modal = {
     title.innerHTML = chartTitle;
     console.log();
 
-    // criando o gráfico
-    Chart.defaults.global.defaultFontColor = "#50525f";
-    var ctx = document.getElementById('chart-zoom').getContext('2d');
-    var myChart = new Chart(ctx, {
+    if (comparison==1) {
+
+      // criando o gráfico
+      Chart.plugins.register(ChartDataLabels);
+      Chart.defaults.global.defaultFontColor = "#50525f";
+      var ctx = document.getElementById('chart-zoom').getContext('2d');
+      var myChart = new Chart(ctx, {
         type: chartType,
         data: {
-            labels: global.dataKeys[chartCod],
-            datasets: [{
-                label: 'Total',
-                data: global.dataValues[chartCod],
-                backgroundColor: 'rgba(154, 219, 249, 0.4)',
-                borderColor: 'rgba(154, 219, 249)',
-                borderWidth: 1,
-            }]
+          labels: global.dataKeys[chartCod],
+          datasets: [{
+              backgroundColor: 'rgba(154, 219, 249, 0.4)',
+              borderColor: 'rgba(154, 219, 249)',
+              borderWidth: 1,
+              data: global.dataValues[chartCod],
+              datalabels: {
+                align: 'bottom',
+                anchor: 'end'
+              }
+          }]
         },
         options: {
-            /* Desativa a legenda */
-            legend: {
-                display: false
-            },
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    },
-                    scaleLabel: {
-                      display: true,
-                      labelString: labelStringY
-                    }
-                }]
+          legend: {
+            display: false
+          },
+          plugins: {
+            datalabels: {
+              color: '#50525f',
+              // Porcentagem
+              formatter: (value, ctx) => {
+                return Math.round(global.dataPorcentage[chartCod][ctx.dataIndex] * 100) + '% do eleitorado da região';
+              }
             }
+          },
+          scales: {
+            xAxes: [{
+              stacked: true
+            }],
+            yAxes: [{
+                stacked: true,
+                ticks: {
+                    beginAtZero: true
+                },
+                scaleLabel: {
+                  display: true,
+                  labelString: labelStringY
+                }
+            }]
+          }
         }
-    });
+      });
+    } else {
+
+      Chart.defaults.global.defaultFontColor = "#50525f";
+      var ctx = document.getElementById('chart-zoom').getContext('2d');
+      var myChart = new Chart(ctx, {
+          type: chartType,
+          data: {
+              labels: global.dataKeys[chartCod],
+              datasets: [{
+                  data: global.dataValues[chartCod],
+                  backgroundColor: 'rgba(154, 219, 249, 0.4)',
+                  borderColor: 'rgba(154, 219, 249)',
+                  borderWidth: 1,
+                  datalabels: {
+                    align: '',
+                    anchor: ''
+                  }
+              }]
+          },
+          options: {
+              //  Desativa a legenda
+              legend: {
+                  display: false
+              },
+              plugins: {
+                datalabels: {
+                  color: '',
+                  font: {},
+                  formatter: {}
+                }
+              },
+              scales: {
+                  yAxes: [{
+                      ticks: {
+                          beginAtZero: true
+                      },
+                      scaleLabel: {
+                        display: true,
+                        labelString: labelStringY
+                      }
+                  }]
+              },
+          }
+      });
+    }
   },
 
   // MODAL DOWNLOAD
